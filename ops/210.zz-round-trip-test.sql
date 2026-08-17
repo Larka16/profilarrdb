a5b5b6b532cd69f5fce10268bcf97d348f73e11e
Devin Blatchford <larka16@proton.me>
2026-08-17 15:28:44 -0700

    Removing duplicate

--- ops/210.zz-round-trip-test.sql
+++
@@ -1,9 +0,0 @@
--- @operation: export
--- @entity: batch
--- @name: zz Round Trip Test
--- @exportedAt: 2026-08-02T00:00:00.000Z
--- @opIds: 12552
-
--- --- BEGIN op 12552 ( create custom_format "zz Round Trip Test" )
-insert into "custom_formats" ("name", "description") values ('zz Round Trip Test', 'Throwaway CF to verify a hand-authored op is applied cleanly by Profilarr. Safe to delete.');
--- --- END op 12552
